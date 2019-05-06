3777395a304e85670044b30a1fa5d58263f23e10
Ihor Mihal <ihor.mihal@gmail.com>
2019-05-07 02:20:14 +0300

    upd

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const node_ssh = require('node-ssh')
 const ssh = new node_ssh()
+
+let monsc = "chat -V -t 5 'ABORT' 'ERROR' 'ABORT' 'NO CARRIER' '' 'AT\\^MONSC' 'OK' > /dev/ttyUSB0 < /dev/ttyUSB0"
  
 ssh.connect({
   host: '192.168.1.1',
@@ -7,11 +9,20 @@ ssh.connect({
   password: '1989'
 })
 .then(() => {
-    return ssh.execCommand('echo "AT^MONSC" > /dev/ttyUSB0')
+    return ssh.execCommand(monsc)
 })
 .then((result) => {
+    console.log('RESULT')
     console.log(result)
 })
 .catch((err) => {
+    console.log('ERROR')
     console.log(err)
 })
+
+let result = { 
+    code: 0,
+    signal: undefined,
+    stdout: '',
+    stderr: 'AT^MONSC\n\n\n^MONSC: WCDMA,255,06,10612,149,47B7E57,3459,-73,-63,-10,6,34014\n\n\n\nOK'
+}
